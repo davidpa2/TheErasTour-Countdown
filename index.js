2df2cb4a0a02e0d2dee92129b8bf6cc1f6406e8b
David Padilla <davidpaag2002@gmail.com>
2024-05-22 21:49:17 +0200

    feat(index): Preparation for the great day!

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Loading countdown');
-
-    const targetDate = new Date('May 30, 2024 00:00:00');
+    // const targetDate = new Date('May 30, 2024 00:00:00');
+    const targetDate = new Date('May 22, 2024 20:46:00');
 
     const spanDays = document.querySelector('span#days');
     const spanHours = document.querySelector('span#hours');
@@ -28,7 +27,49 @@ document.addEventListener('DOMContentLoaded', () => {
         spanHours.textContent = remainingHours;
         spanMinutes.textContent = remainingMinutes;
         spanSeconds.textContent = remainingSeconds;
+
+        console.log(duration);
+        // if (remainingSeconds == 59) {
+        if (duration <= 0) {
+            manageElements();
+            loadFireworks();
+        }
     }
+
+    function manageElements() {
+        let dateDiv = document.getElementById("dateDiv");
+        dateDiv.setAttribute("class", "dateDivAnimation");
+
+        dateDiv.childNodes.forEach(function (item) {
+            if (item.textContent.trim() != "") {
+                console.log(item.textContent);
+                item.textContent = "THE ERAS TOUR";
+            }
+        })
+
+        let timerP = document.getElementById("timerP");
+        timerP.setAttribute("class", "hideElement");
+        // timerP.replaceChildren("Ha llegado el día");
+        
+        let simpleTitle = document.getElementById("simpleTitle");
+        simpleTitle.setAttribute("class", "hideElement");
+
+        let dreamP = document.getElementById("dreamP");
+        dreamP.textContent = "No puede ser, esto es increíble... ¡Ha llegado el día!"
+        // timerP.replaceChildren("Ha llegado el día");
+        // dreamP.setAttribute("class", "hideElement");
+    }
+
+    function loadFireworks() {
+        setTimeout(() => {
+            let styleSheet = document.createElement('link');
+            styleSheet.rel = "stylesheet";
+            styleSheet.href = "fireworks.css";
+            document.body.append(styleSheet);
+            document.body.classList.add('dark')
+        }, 4000)
+    }
+
 
     updateCountdown();
 
